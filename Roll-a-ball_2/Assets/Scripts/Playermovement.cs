6a02dce7565b314828e9d21c14743102da8c337e
AdharshG <34604785+Adharshg@users.noreply.github.com>
2018-08-07 15:29:34 +0530

    Initial commit

---
+++ Roll-a-ball_2/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Playermovement : MonoBehaviour {
+	public Rigidbody rb;
+	public Vector3 jump;
+	public float fforce=2000f;
+	public float playerJumpHeight = 2.0f;
+	public float sforce=500f;
+	void Start (){
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+
+			rb.AddForce(0,0,fforce*Time.deltaTime);
+			
+		if(Input.GetKey("d")){
+			rb.AddForce(sforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+		}
+		
+		if(Input.GetKey("a")){
+			rb.AddForce(-sforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+		}
+		if(Input.GetKeyDown("w")){
+			rb.AddForce(jump * playerJumpHeight, ForceMode.Impulse);
+		}
+	}
+}
